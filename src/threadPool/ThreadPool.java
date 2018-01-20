8956952601455ec9c3739fc4eb1fda338b7e6fa7
hhh <957400829@qq.com>
2018-01-20 23:26:17 +0800

    threadPool

---
+++ src/threadPool/ThreadPool.java
@@ -0,0 +1,66 @@
+package threadPool;
+
+
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by xiongxiaoyu on 2018/1/19.
+ */
+public class ThreadPool {
+	private static ThreadPool instance=null;
+	//空闲的线程总数
+	private List<Worker> idleThreads;
+	//已有的线程总数
+	private int threadCounter;
+	private boolean isShutDown=false;
+
+	private ThreadPool(){
+		this.idleThreads=new Vector(5);
+		threadCounter=0;
+	}
+	public int getCreatedThreadsCount(){
+		return threadCounter;
+	}
+
+	//取得线程池实例
+	public synchronized static ThreadPool getInstance(){
+		if (instance==null)
+			instance=new ThreadPool();
+		return instance;
+	}
+
+	//将线程放入池中
+	protected synchronized void repool(Worker repollingThread){
+		if (!isShutDown){
+			idleThreads.add(repollingThread);
+		}else {
+			repollingThread.shutDown();
+		}
+	}
+	public synchronized void shutdown(){
+		isShutDown=true;
+		for (int threadIndex = 0; threadIndex < idleThreads.size(); threadIndex++) {
+			Worker idleThread=(Worker) idleThreads.get(threadIndex);
+			idleThread.shutDown();
+		}
+	}
+	public synchronized void start(Runnable target){
+		Worker thread=null;
+		//如果有空闲线程直接使用
+		if (idleThreads.size()>0){
+			int lastIndex=idleThreads.size()-1;
+			thread=(Worker)idleThreads.get(lastIndex);
+			idleThreads.remove(lastIndex);
+			//立即执行这个任务
+			thread.setTarget(target);
+		}else {
+			threadCounter++;
+			//创建新线程
+			thread=new Worker(target,"PThread #"+threadCounter,this) ;
+			thread.start();
+		}
+
+	}
+}
---
+++ src/threadPool/Worker.java
@@ -0,0 +1,56 @@
+package threadPool;
+
+/**
+ * Created by xiongxiaoyu on 2018/1/19.
+ */
+public class Worker extends Thread{
+	//线程池
+	private ThreadPool pool;
+	//任务
+	private Runnable target;
+	private boolean isShutDown=false;
+	private boolean isIdle=false;
+
+	public Worker(Runnable target,String name,ThreadPool pool){
+		super(name);
+		this.pool=pool;
+		this.target=target;
+	}
+	public Runnable getTarget(){
+		return  target;
+	}
+	public  boolean isIdle(){
+		return isIdle;
+	}
+	public void run(){
+		while (!isShutDown){
+			isIdle=false;
+			if (target!=null){
+				//运行任务
+				target.run();
+			}
+			//任务结束后
+			isIdle=true;
+			try{
+				//该任务结束后，不关闭线程，而是放入线程池空闲列队
+				pool.repool(this);
+				synchronized (this){
+					//线程空闲，等待心得任务到来
+					wait();
+				}
+			} catch (InterruptedException e) {
+//				e.printStackTrace();
+			}
+			isIdle=false;
+		}
+	}
+	public synchronized void setTarget(Runnable newTarget){
+		target=newTarget;
+		notifyAll();
+	}
+
+	public synchronized void shutDown(){
+		isShutDown=true;
+		notifyAll();
+	}
+}
